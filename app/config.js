da0fbbb8fc895b8123f83b88727701d8c7878610
Jens <jens.elstner@technicallyfeasible.com>
2016-02-08 07:53:07 +0000

    Added config file

---
+++ app/config.js
@@ -0,0 +1,6 @@
+export default {
+  programEndpoint: 'https://api.particle.io/v1/devices/{deviceId}/program?access_token={accessToken}',
+  infoEndpoint: 'https://api.particle.io/v1/devices/{deviceId}/info?access_token={accessToken}',
+  deviceId: '54ff74066678574930510867',
+  accessToken: '4e79498c465c80fe99cc97ac8545f36e65bbb2ea',
+};
